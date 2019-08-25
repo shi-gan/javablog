839e393226b99a18a6113d2539822c3ff12177fd
nizhipeng <1174740748@qq.com>
2019-08-25 13:26:16 +0800

    //  主干再修改

--- src/main/java/cn/tycoding/controller/AdminController.java
+++ src/main/java/cn/tycoding/controller/AdminController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpSession;
 
 /**
- * github测试 远程
+ * github测试 远程  这是主干
  * 管理人员的Controller层
  *
  * @author TyCoding
